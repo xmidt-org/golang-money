34bd54bcb55675596a0fcf265801e8db4fb4b35a
Ananthakrishnan <ramki_ananthakrishnan@cable.comcast.com>
2018-06-21 13:49:48 -0700

    Added Tracker String unit test.

--- tracker_test.go
+++ tracker_test.go
@@ -12,6 +12,7 @@ import (
 func TestHTTPTracker(t *testing.T) {
 	t.Run("Start", testHTTPTrackerStart)
 	t.Run("Finish", testHTTPTrackerFinish)
+	t.Run("String", testHTTPTrackerString)
 }
 
 func testHTTPTrackerStart(t *testing.T) {
@@ -77,7 +78,7 @@ func testHTTPTrackerFinish(t *testing.T) {
 			SID: 11,
 			PID: 11,
 		},
-		Success:   true,
+		Success:   false,
 		Code:      200,
 		StartTime: startTime,
 		Duration:  time.Second,
@@ -98,4 +99,28 @@ func testHTTPTrackerFinish(t *testing.T) {
 	assert.Equal(h.span.Code, r.Code)
 	assert.Equal(h.span.Err, r.Err)
 	assert.Equal(h.span.Success, r.Success)
+}
+
+func testHTTPTrackerString(t *testing.T) {
+	startTime, e := time.Parse(time.RFC3339, "1970-01-01T00:00:01+00:00") //1 second into epoch time = 1,000,000 microseconds
+	require.Nil(t, e)
+
+	i := &Span{
+		Name:    "test-span",
+		AppName: "test-app",
+		TC: &TraceContext{
+			TID: "test-trace",
+			SID: 1,
+			PID: 1,
+		},
+		Success:   true,
+		Code:      200,
+		StartTime: startTime,
+		Duration:  time.Second,
+		Host:      "localhost",
+	}
+
+	var expected = "span-name=test-span;app-name=test-app;span-duration=1000000;span-success=true;span-id=1;trace-id=test-trace;parent-id=1;start-time=1000000;host=localhost;http-response-code=200"
+
+	assert.Equal(t, i.String(), expected)
 }
