be1f2f89dce0a913813911085578ebd3c89df694
Ananthakrishnan <ramki_ananthakrishnan@cable.comcast.com>
2018-06-22 11:43:48 -0700

    Added more WIP Spanner unit test.

--- spanner_test.go
+++ spanner_test.go
@@ -1,6 +1,7 @@
 package money
 
 import (
+	"context"
 	"testing"
 	"time"
 
@@ -16,9 +17,13 @@ func testStart(t *testing.T) {
 	startTime, e := time.Parse(time.RFC3339, "1970-01-01T00:00:01+00:00") //1 second into epoch time = 1,000,000 microseconds
 	require.Nil(t, e)
 
-	i := &Span{
+	c, cancel := context.WithCancel(context.Background())
+	require.NotNil(t, c)
+	defer cancel()
+
+	s := Span{
 		Name:    "test-span",
-		AppName: "test-app",
+		AppName: "test-start-app",
 		TC: &TraceContext{
 			TID: "test-trace",
 			SID: 1,
@@ -30,8 +35,28 @@ func testStart(t *testing.T) {
 		Duration:  time.Second,
 		Host:      "localhost",
 	}
+	require.NotNil(t, s)
 
-	var expected = "span-name=test-span;app-name=test-app;span-duration=1000000;span-success=true;span-id=1;trace-id=test-trace;parent-id=1;start-time=1000000;host=localhost;http-response-code=200"
+	h := new(HTTPTracker)
+	h.span = Span{
+		Name:    "test-tracker-span",
+		AppName: "test-app",
+		TC: &TraceContext{
+			TID: "test-tracker-trace",
+			SID: 11,
+			PID: 11,
+		},
+		Success:   true,
+		Code:      200,
+		StartTime: startTime,
+		Duration:  time.Second,
+		Host:      "localhost",
+	}
 
-	assert.Equal(t, i.String(), expected)
+	var end HTTPTracker
+	/* Following call results in panic.
+	 * TODO - Need clarification on the following. Start is a method call of pointer receiver HTTPTracker and returns a HTTPTracker
+	 */
+	//end := h.Start(c, s)
+	assert.NotNil(t, end)
 }
